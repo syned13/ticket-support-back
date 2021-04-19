57a208fc5d3119c7a3d239c77beb0277b6cd3546
Denys <drosario@truora.com>
2021-04-18 20:26:35 -0400

    add http handlers stubs

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/syned13/ticket-support-back
 go 1.16
 
 require (
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jackc/pgx/v4 v4.11.0 // indirect
 	github.com/pashagolub/pgxmock v0.0.0-20210329125448-d2e8b19a1746 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
---
+++ internal/handlers/auth/http.go
@@ -0,0 +1,37 @@
+package handlers
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	authService "github.com/syned13/ticket-support-back/internal/service/auth"
+)
+
+type HTTPHandler interface {
+	HandleLogin(ctx context.Context) http.HandlerFunc
+	HandleSignup(ctx context.Context) http.HandlerFunc
+}
+
+type httpHandler struct {
+	service authService.Service
+}
+
+func SetupRoutes(ctx context.Context, service authService.Service, router mux.Router) {
+	handler := httpHandler{service: service}
+
+	router.HandleFunc("/login", handler.HandleLogin(ctx)).Methods(http.MethodPost)
+	router.HandleFunc("/signup", handler.HandleSignup(ctx)).Methods(http.MethodPost)
+}
+
+func (h httpHandler) HandleLogin(ctx context.Context) http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func (h httpHandler) HandleSignup(ctx context.Context) http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+
+	}
+}
